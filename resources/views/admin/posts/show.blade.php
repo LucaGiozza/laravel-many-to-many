6d7cc29b2ed2ee4ad88199f9ed8b94225f223b92
Luca Giozza <luca.giozza@gmail.com>
2021-09-16 16:14:37 +0200

    tabella ponte

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -8,6 +8,10 @@
       {{$post->title}}
      </div>
      <div class="card-body">
+       @if($post->category)
+       <h3>Category: {{$post->category->name}}</h3>
+
+       @endif
       <h5 class="card-title">{{$post->slug}}</h5>
       <p class="card-text">{{$post->content}}</p>
       
