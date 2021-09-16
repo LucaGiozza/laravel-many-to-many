6f368b4c0d9959f779c64ff08fb22267ec95f5f9
Luca Giozza <luca.giozza@gmail.com>
2021-09-16 15:05:31 +0200

    first commit

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts/app')
+
+@section('content')
+
+<div class="container">
+  <!-- primo metodo -->
+ @if($errors->any())
+
+<div class="alert alert-danger">
+
+
+    <ul>
+       <h4>Attenzione!!</h4>
+        @foreach($errors->all() as $error )
+        
+
+        <li>{{$error}}</li>
+   
+
+       @endforeach
+     </ul>
+</div>
+
+@endif 
+
+
+
+
+<form action="{{route('admin.posts.store')}}" method="post">
+    @csrf
+  <div class="mb-3">
+      <label for="titolo" class="form-label">Titolo</label>
+      <input type="text" class="form-control @error('title') is-invalid @enderror"  id="titolo" name="title" value="{{old('title')}}">
+      
+  </div>
+  <div class="mb-3">
+      <label for="cat" class="form-label">Categoria</label>
+      <select name="category_id" id="cat">
+
+
+         <option value="">Seleziona una categoria..</option>
+         @foreach($categories as $category)
+          <option value="{{$category->id}}">{{$category->name}}</option> 
+
+          @if($category->id == old('category_id'))selected
+
+          @endif
+
+
+         @endforeach
+
+      </select>
+      
+      
+  </div>
+  <div class="mb-3">
+     <label for="desc" class="form-label">descrizione</label>
+     <textarea class="form-control @error('content') is-invalid @enderror" name="content" id="desc" cols="30" rows="10">{{old('content')}}</textarea>
+     
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+@endsection
